3f295f6829989f7e2bd45002374e3374774b1e58
Martin Komischke <martin@6wunderkinder.com>
2013-07-29 15:43:04 +0200

    * Initial commit.

---
+++ Tests/VectorClockFixture.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace VectorClock.Tests
+{
+    public class VectorClockFixture
+    {
+        [Fact]
+        public void There_should_be_3_resulting_items()
+        {
+            var one = new VectorClock();
+            one.Tick("foo");
+            one.Tick("bar");
+
+            var other = new VectorClock();
+            other.Tick("baz");
+            other.Tick("baz");
+            other.Tick("bar");
+            other.Tick("foo");
+
+            var result = VectorClock.Merge(one, other);
+
+            Assert.Equal(3, result.Keys.Length);
+        }
+    }
+}
---
+++ VectorClock/VectorClock.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VectorClock
+{
+    public class VectorClock
+    {
+        private readonly Dictionary<string, int> _items;
+
+        public int DefaultValue = 0;
+
+        public VectorClock()
+        {
+            _items = new Dictionary<string, int>();
+        }
+
+        public VectorClock(VectorClock other) : this(other._items) {}
+
+        public VectorClock(Dictionary<string, int> dictionary)
+        {
+            _items = dictionary.ToDictionary(e => e.Key, e => e.Value);;
+        }
+
+        public void Tick(string key)
+        {
+            if (_items.ContainsKey(key))
+                _items[key] = _items[key] + 1;
+            else
+                _items.Add(key, 1); // starts at 0, +1
+        }
+
+        public string[] Keys
+        {
+            get { return _items.Keys.ToArray(); }
+        }
+
+        public int[] Values
+        {
+            get { return _items.Values.ToArray(); }
+        }
+
+        public int GetValue(string key)
+        {
+            return _items.ContainsKey(key) ? _items[key] : DefaultValue;
+        }
+
+        public bool Contains(string key)
+        {
+            return _items.ContainsKey(key);
+        }
+
+        private void Add(string key, int value)
+        {
+            _items.Add(key, value);
+        }
+
+        private bool IsDefaultValue(string key)
+        {
+            return GetValue(key) == DefaultValue;
+        }
+
+        public static VectorClock Merge(VectorClock one, VectorClock other)
+        {
+            var result = new VectorClock(one);
+
+            foreach (var k in other.Keys)
+            {
+                if (!result.Contains(k) || result.GetValue(k) < other.GetValue(k))
+                {
+                    result.Add(k, other.GetValue(k));
+                }
+            }
+
+            return result;
+        }
+
+        public enum VectorComparision
+        {
+            Greater,    // if one > other
+            Equal,      // if one == other
+            Smaller,    // if one < other
+            Simultanous // if one <> other
+        }
+
+        public VectorComparision Compare(VectorClock one, VectorClock other)
+        {
+            var equal = true;
+            var greater = true;
+            var smaller = true;
+
+            foreach (var k in one.Keys)
+            {
+                var oneValue = one.GetValue(k);
+
+                if (other.Contains(k))
+                {
+                    var otherValue = other.GetValue(k);
+
+                    if (oneValue < otherValue)
+                    {
+                        equal = false;
+                        greater = false;
+                    }
+
+                    if (oneValue > otherValue)
+                    {
+                        equal = false;
+                        smaller = false;
+                    }
+                }
+                else if (oneValue != DefaultValue)
+                {
+                    equal = false;
+                    smaller = false;
+                }
+            }
+
+            foreach (var k in other.Keys)
+            {
+                if (!one.Contains(k) && !other.IsDefaultValue(k))
+                {
+                    equal = false;
+                    greater = false;
+                }
+            }
+
+            if (equal)
+                return VectorComparision.Equal;
+            if (greater)
+                return VectorComparision.Greater;
+            if (smaller)
+                return VectorComparision.Smaller;
+            else
+                return VectorComparision.Simultanous;
+        }
+    }
+}
